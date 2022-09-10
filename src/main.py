734f12f66057904602274d69e0a375a61a8629ef
WillyBBlue <skihatduckie@gmail.com>
2022-09-09 20:22:35 -0400

    Add CLI

--- src/main.py
+++ src/main.py
@@ -1,2 +1,11 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='A simple example of linear regression.')
+parser.add_argument('--verbose', action='store_true', help='Enable verbose output')
+
+args = parser.parse_args()
+
 if __name__ == "__main__":
+    if args.verbose:
+        print("Verbose Hello!")
     print("Hello World!")
